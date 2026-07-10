f76b6c59149a21784d687adaa21ab946e3236730
epicstuudio <ahamxa007@gmail.com>
2026-07-10 19:12:14 +0400

    chore: Update analytics component

--- components/analytics/Analytics.tsx
+++ components/analytics/Analytics.tsx
@@ -11,35 +11,16 @@ export function Analytics() {
     return null;
   }
 
-  const gaId = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID || 'G-34E8FVSN17';
-  const clarityId = process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID || 'wnrurgeyjs';
-
   return (
     <>
-      {/* Google Analytics 4 */}
-      <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${gaId}`}
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
+      {/* Google Tag Manager */}
+      <Script id="google-tag-manager" strategy="afterInteractive">
         {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${gaId}', {
-            page_path: window.location.pathname,
-          });
-        `}
-      </Script>
-
-      {/* Microsoft Clarity */}
-      <Script id="microsoft-clarity" strategy="afterInteractive">
-        {`
-          (function(c,l,a,r,i,t,y){
-            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-          })(window, document, "clarity", "script", "${clarityId}");
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-T2NP7DBH');
         `}
       </Script>
     </>
